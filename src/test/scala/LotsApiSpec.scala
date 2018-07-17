61e66e618b8f5fa2b48168be3d6fcdc02f0c473c
Michal Kreft <yomajkel@gmail.com>
2018-07-17 08:35:41 +0200

    add tests for GET lots call

--- src/test/scala/LotsApiSpec.scala
+++ src/test/scala/LotsApiSpec.scala
@@ -1,13 +1,22 @@
 import java.util.UUID
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, MediaTypes, StatusCodes}
-import akka.http.scaladsl.server.UnsupportedRequestContentTypeRejection
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, FromResponseUnmarshaller, PredefinedFromEntityUnmarshallers, Unmarshaller}
+import akka.stream.Materializer
+import akka.util.ByteString
+import entities.{Auction, Lot}
+import mappings.JsonMappings
 import org.scalatest.{Matchers, WordSpec}
+import persistence.LimitedResult
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
 class LotsApiSpec extends WordSpec with Matchers with ScalatestRouteTest with Routes {
+
+  implicit val limResLotUm = jsonFormat4(LimitedResult[Lot])
+
   "Lots Api" should {
 
     "return a created object id given valid POST data" in {
@@ -38,5 +47,38 @@ class LotsApiSpec extends WordSpec with Matchers with ScalatestRouteTest with Ro
         status shouldEqual StatusCodes.BadRequest
       }
     }
+
+    "return a result with default limit and offset given a valid auction id" in {
+      Get("/lots?auctionId=4ac772c5-bc52-4d3c-ba9e-4010f511e175") ~> lotsApi ~> check {
+        status shouldEqual StatusCodes.OK
+        contentType shouldEqual ContentTypes.`application/json`
+
+        val limitedResult = entityAs[LimitedResult[Lot]]
+
+        limitedResult.limit shouldEqual 10
+        limitedResult.offset shouldEqual 0
+
+        limitedResult.total shouldEqual 3
+        limitedResult.items.length shouldEqual 3
+      }
+    }
+
+    "return a result with limit and offset given in a request" in {
+      Get("/lots?auctionId=4ac772c5-bc52-4d3c-ba9e-4010f511e175&limit=1&offset=2") ~> lotsApi ~> check {
+        status shouldEqual StatusCodes.OK
+        contentType shouldEqual ContentTypes.`application/json`
+
+        val limitedResult = entityAs[LimitedResult[Lot]]
+
+        limitedResult.limit shouldEqual 1
+        limitedResult.offset shouldEqual 2
+
+        limitedResult.total shouldEqual 3
+        limitedResult.items.length shouldEqual 1
+
+        limitedResult.items.head.id shouldEqual "2e5faabf-47eb-40c1-a961-b1ca7e928b49"
+      }
+    }
+
   }
 }
