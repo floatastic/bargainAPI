02bef8258b236c823e7c3df50a237c309f4015ad
Michal Kreft <yomajkel@gmail.com>
2018-07-30 09:28:49 +0200

    remove data creation from migrations

--- src/main/resources/db/migration/V2__Add_auctions.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "auctions" ("id", "data") VALUES ('4ac772c5-bc52-4d3c-ba9e-4010f511e175', 'First');
-INSERT INTO "auctions" ("id", "data") VALUES ('0ae024c0-0ddb-4e59-9e2c-721a27c386f6', 'Second');
-INSERT INTO "auctions" ("id", "data") VALUES ('b9549a8e-be47-4b8b-a38c-226c2647073d', 'Third');
--- src/main/resources/db/migration/V4__Add_lots.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "lots" ("id", "data", "auction_id") VALUES ('6b2e9336-7911-4381-a0e7-382a4e510291', 'Lot 1 for auction 1', '4ac772c5-bc52-4d3c-ba9e-4010f511e175');
-INSERT INTO "lots" ("id", "data", "auction_id") VALUES ('826a8ebb-8c9c-45b7-b08e-c784c442f55b', 'Lot 2 for auction 1', '4ac772c5-bc52-4d3c-ba9e-4010f511e175');
-INSERT INTO "lots" ("id", "data", "auction_id") VALUES ('2e5faabf-47eb-40c1-a961-b1ca7e928b49', 'Lot 3 for auction 1', '4ac772c5-bc52-4d3c-ba9e-4010f511e175');
